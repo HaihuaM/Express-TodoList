c495aaf0aeffa77de01cabe3f787676f8854ec2a
HaihuaM <haihuam@outlook.com>
2018-07-20 18:39:52 +0800

    Add files

---
+++ app.js
@@ -0,0 +1,48 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+// var bodyParser = require('body-parser')
+require('./db')
+
+var routes = require('./routes');
+// var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+// app.use(bodyParser);
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', routes.index);
+app.post( '/create', routes.create );
+app.post( '/update/:id', routes.update );
+app.get( '/destroy/:id', routes.destroy );
+app.get( '/edit/:id', routes.edit );
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
